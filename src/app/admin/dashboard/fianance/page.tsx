3b2e2af0ad726510a313a65fbb91953f2817838d
EphriamHab <ephremhabtamu2015@gmail.com>
2025-01-16 19:35:07 +0300

    fianance

---
+++ src/app/admin/dashboard/fianance/page.tsx
@@ -0,0 +1,14 @@
+
+
+  export default function Finance(){
+    return (
+        <div className="flex flex-col  justify-center items-center p-2 w-1/2 bg-slate-50  rounded-lg shadow">
+            <h1  className="text-lg text-gray-700">This is the Finance  page </h1>
+            <p className="text-gray-700 p-4 ">
+              This is the fiance page for out application it shows a list of  
+              transaction  based on the user story and show the most quailefies
+               tyeo od josb in the domain area of  that  what  ever the weather shows take
+            </p>
+        </div>
+    );
+  }
