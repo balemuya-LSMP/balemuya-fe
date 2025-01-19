9701c6bfcd08f64ebe2acf60c812634c6422c9c1
EphriamHab <ephremhabtamu2015@gmail.com>
2025-01-19 19:13:22 +0300

    auth api

---
+++ src/store/api/auth.api.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { UserResponse, User, loginState, loginResponse } from '../types';
+import { log } from 'console';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://balemuya-project.vercel.app/api/users' }),
+  endpoints: (builder) => ({
+    registerUser: builder.mutation<UserResponse, User>({
+      query: (user) => {
+        const { confirmPassword, ...userWithoutConfirmPassword } = user;
+        return {
+          url: '/auth/register/',
+          method: 'POST',
+          body: userWithoutConfirmPassword,
+        };
+      },
+    }),
+    loginUser: builder.mutation<loginResponse, loginState>({
+      query: (user) => {
+        return {
+          url: '/auth/login/',
+          method: 'POST',
+          body: user,
+        };
+      },
+    }),
+    googleLogin: builder.mutation<loginResponse, { code: string; state: string }>({
+      query: ({ code, state }) => ({
+        url: `/auth/google-signin/?code=${encodeURIComponent(code)}&state=${encodeURIComponent(state)}`, 
+        method: 'GET', 
+      }),
+    }),
+    logoutUser: builder.mutation<null,  string >({
+      query: (token) => ({
+        url: '/auth/logout/',
+        method: 'POST',
+        body: token,
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterUserMutation, useLoginUserMutation, useGoogleLoginMutation, useLogoutUserMutation} = apiSlice;
