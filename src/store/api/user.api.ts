c69de629aae77430e47e8e9ac81ca806f0e19eb6
EphriamHab <ephremhabtamu2015@gmail.com>
2025-01-19 19:13:32 +0300

    user api

---
+++ src/store/api/user.api.ts
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://balemuya-project.vercel.app/api/users",
+  }),
+  endpoints: (builder) => ({
+    listProfessionals: builder.query({
+      query: (status) => {
+        const params = status ? `?status=${status}` : "";
+        return `/professionals/${params}`;
+      },
+    }),
+    listCustomers: builder.query({
+      query: (status) => {
+        const params = status ? `?status=${status}` : "";
+        return `/customers/${params}`;
+      },
+    }),
+    listAdmins: builder.query({
+      query: (status) => {
+        const params = status ? `?status=${status}` : "";
+        return `/admins/${params}`;
+      },
+    }),
+  }),
+});
+
+export const {
+  useListProfessionalsQuery,
+  useListCustomersQuery,
+  useListAdminsQuery,
+} = userApi;
