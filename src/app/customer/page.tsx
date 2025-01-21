762d8b4d0cfc819228a1cad07c783aa655d70ce2
EphriamHab <ephremhabtamu2015@gmail.com>
2025-01-21 23:55:55 +0300

    customer

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -8,9 +8,12 @@ import {
 import { MdPayment } from "react-icons/md";
 import { FiArrowRight, FiClipboard } from "react-icons/fi";
 import { HiOutlineBriefcase } from "react-icons/hi";
+import { FaLocationDot } from "react-icons/fa6";
+
 import Image from "next/image";
 import React from "react";
 import Footer from "../(features)/_components/footer";
+import Header from "./_components/header";
 
 const workSamples = [
   {
@@ -73,36 +76,7 @@ const workSamples = [
 export default function Home() {
   return (
     <div className="bg-gray-100 font-sans no-scrollbar">
-      {/* Navbar */}
-      <header className="bg-white shadow">
-        <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-          <h1 className="text-2xl font-bold text-purple-700">BALEMUYA</h1>
-          <nav className="flex space-x-6">
-            <a href="#" className="text-gray-700 hover:text-purple-700">
-              Home
-            </a>
-            <a href="#" className="text-gray-700 hover:text-purple-700">
-              Professionals
-            </a>
-            <a href="#" className="text-gray-700 hover:text-purple-700">
-              Work Post
-            </a>
-          </nav>
-          <div className="w-10 h-10 rounded-full bg-gray-300 flex items-center justify-center">
-            <Image
-              src="/images/user.jpg"
-              alt="User"
-              width={40}
-              height={40}
-              className="rounded-full"
-            />
-
-            <div className="w-3 h-3 bg-green-500 rounded-full absolute top-0 right-0">
-              Abebe
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       {/* Hero Section */}
       <section
@@ -165,25 +139,31 @@ export default function Home() {
               className="bg-gray-100 p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
             >
               <div className="flex justify-between items-center">
-                <div className="flex justify-center gap-8 items-center">
+                <div className="flex justify-center gap-2 items-center">
                   <Image
                     src={work.image}
                     alt={work.title}
-                    width={80}
-                    height={80}
+                    width={45}
+                    height={45}
                     className="rounded-full"
                   />
-                  <p>{work.name}</p>
+                  <p className="tetx-lg font-semibold ">{work.name}</p>
                 </div>
+                <span className="text-yellow-500">★★★★★</span>
               </div>
-
+              <div className="flex justify-start items-center ml-3 mt-2 gap-2">
+                <FaLocationDot className="text-purple-700" />
+                <p>2km Away</p>
+              </div>
               <h4 className="text-lg font-bold mt-4 text-gray-800">
                 {work.title}
               </h4>
               <p className="text-gray-600 mt-2 text-sm">{work.description}</p>
-              <button className="mt-4 px-4 py-2 bg-purple-700 text-white rounded-lg hover:bg-purple-800">
-                Contact Now
-              </button>
+              <div className="flex justify-end items-center">
+                <button className="mt-4 mr-0 px-4 py-2 text-purple-700">
+                  Details
+                </button>
+              </div>
             </div>
           ))}
         </div>
