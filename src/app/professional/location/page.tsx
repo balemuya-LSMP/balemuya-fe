0fa243e68b8f433bf7791a54fff460682479c993
EphriamHab <ephremhabtamu2015@gmail.com>
2025-01-24 21:10:31 +0300

    location

---
+++ src/app/professional/location/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import MapComponent from "@/app/(features)/_components/map";
+
+
+export default function ProfessionalLocation() {
+
+    return(
+        <div>
+            <MapComponent/>
+        </div>
+    )
+}
