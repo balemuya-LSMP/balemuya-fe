1ce3dbece6939a045ff61971013a7734a507b99f
EphriamHab <ephremhabtamu2015@gmail.com>
2025-01-24 10:51:20 +0300

    api modified

--- src/store/api/userProfile.api.ts
+++ src/store/api/userProfile.api.ts
@@ -14,14 +14,17 @@ export const userProfileApi = createApi({
       return headers;
     },
   }),
-  tagTypes: ["UserProfile"], 
+  tagTypes: ["UserProfile"],
   endpoints: (builder) => ({
     userProfile: builder.query({
       query: () => "/profile/",
-      providesTags: ["UserProfile"], 
+      providesTags: ["UserProfile"],
     }),
 
-    updateProfile: builder.mutation<UserResponse, { updated: Record<string, any> }>({
+    updateProfile: builder.mutation<
+      UserResponse,
+      { updated: Record<string, any> }
+    >({
       query: ({ updated }) => ({
         url: `/profile/update/`,
         method: "PUT",
@@ -29,7 +32,204 @@ export const userProfileApi = createApi({
       }),
       invalidatesTags: ["UserProfile"],
     }),
+    addAddresses: builder.mutation<
+      UserResponse,
+      { addresses: Record<string, any> }
+    >({
+      query: ({ addresses }) => ({
+        url: `/profile/address/create/`,
+        method: "POST",
+        body: addresses,
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    updateAddresses: builder.mutation<
+      UserResponse,
+      { id: string; addresses: Record<string, any> }
+    >({
+      query: ({ id, addresses }) => ({
+        url: `/profile/addresses/${id}/`,
+        method: "PUT",
+        body: addresses,
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    removeAddresses: builder.mutation<UserResponse, { id: string }>({
+      query: ({ id }) => ({
+        url: `/profile/addresses/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+
+    addCertificates: builder.mutation<
+      UserResponse,
+      { certificates: Record<string, any> }
+    >({
+      query: ({ certificates }) => ({
+        url: `/professional/profile/certificates/add/`,
+        method: "POST",
+        body: certificates,
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    updateCertificates: builder.mutation<
+      UserResponse,
+      {id:string, certificates: Record<string, any> }
+    >({
+      query: ({id, certificates }) => ({
+        url: `/professional/profile/certificates/${id}/update/`,
+        method: "PUT",
+        body: certificates,
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    deleteCertificates: builder.mutation<
+      UserResponse,
+      { id:string }
+    >({
+      query: ({ id }) => ({
+        url: `/professional/profile/certificates/${id}/delete/`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    addEducations: builder.mutation<
+      UserResponse,
+      { educations: Record<string, any> }
+    >({
+      query: ({ educations }) => ({
+        url: `/professional/profile/educations/add/`,
+        method: "POST",
+        body: educations,
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    updateEducations: builder.mutation<
+      UserResponse,
+      {id:string, educations: Record<string, any> }
+    >({
+      query: ({id, educations }) => ({
+        url: `/professional/profile/educations/${id}/update/`,
+        method: "PUT",
+        body: educations,
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    deleteEducations: builder.mutation<
+      UserResponse,
+      { id: string }
+    >({
+      query: ({ id }) => ({
+        url: `/professional/profile/educations/${id}/delete/`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    addPortifolios: builder.mutation<
+      UserResponse,
+      { portifolios: Record<string, any> }
+    >({
+      query: ({ portifolios }) => ({
+        url: `/professional/profile/portfolios/add/`,
+        method: "POST",
+        body: portifolios,
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    updatePortifolios: builder.mutation<
+      UserResponse,
+      {id:string, portifolios: Record<string, any> }
+    >({
+      query: ({id, portifolios }) => ({
+        url: `/professional/profile/portfolios/${id}/update/`,
+        method: "PUT",
+        body: portifolios,
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    deletePortifolios: builder.mutation<
+      UserResponse,
+      { id: string }
+    >({
+      query: ({ id }) => ({
+        url: `/professional/profile/portfolios/${id}/delete/`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    addSkills: builder.mutation<UserResponse, { skills: Record<string, any> }>({
+      query: ({ skills }) => ({
+        url: `professional/profile/skills/`,
+        method: "POST",
+        body: skills,
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    deleteSkills: builder.mutation<
+      UserResponse,
+      { skills: Record<string, any> }
+    >({
+      query: ({ skills }) => ({
+        url: `professional/profile/${skills}/`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    addCategories: builder.mutation<
+      UserResponse,
+      { categories: Record<string, any> }
+    >({
+      query: ({ categories }) => ({
+        url: `/professional/profile/categories/     `,
+        method: "POST",
+        body: categories,
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    deleteCategories: builder.mutation<
+      UserResponse,
+      { categories: Record<string, any> }
+    >({
+      query: ({ categories }) => ({
+        url: `professional/profile/${categories}/`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["UserProfile"],
+    }),
+    updateProfessionalProfile: builder.mutation<
+    UserResponse,
+    { updated: Record<string, any> }
+  >({
+    query: ({ updated }) => ({
+      url: `/professional/profile/update/`,
+      method: "PUT",
+      body: updated,
+    }),
+    invalidatesTags: ["UserProfile"],
+  }),
   }),
 });
 
-export const { useUserProfileQuery, useUpdateProfileMutation } = userProfileApi;
+export const {
+  useUserProfileQuery,
+  useUpdateProfileMutation,
+  useAddAddressesMutation,
+  useAddCertificatesMutation,
+  useAddEducationsMutation,
+  useAddPortifoliosMutation,
+  useAddSkillsMutation,
+  useDeleteCertificatesMutation,
+  useDeleteEducationsMutation,
+  useDeletePortifoliosMutation,
+  useDeleteSkillsMutation,
+  useRemoveAddressesMutation,
+  useUpdateAddressesMutation,
+  useUpdateCertificatesMutation,
+  useUpdateEducationsMutation,
+  useLazyUserProfileQuery,
+  useUpdatePortifoliosMutation,
+  useUpdateProfessionalProfileMutation,
+  useAddCategoriesMutation,
+  useDeleteCategoriesMutation,
+} = userProfileApi;
