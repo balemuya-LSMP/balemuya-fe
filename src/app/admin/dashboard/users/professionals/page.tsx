172543ff1c5125d6a522d495ddb41921a3c6497c
EphriamHab <ephremhabtamu2015@gmail.com>
2025-01-25 20:39:50 +0300

    professionals

--- src/app/admin/dashboard/users/professionals/page.tsx
+++ src/app/admin/dashboard/users/professionals/page.tsx
@@ -105,6 +105,7 @@ export default function Users() {
                   >
                     Blocked
                   </li>
+                  
                 </ul>
               </div>
             )}
