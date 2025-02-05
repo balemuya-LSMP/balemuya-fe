dbf26fab4d3c46428a189d3f1b39f1539fec12ef
EphriamHab <ephremhabtamu2015@gmail.com>
2025-02-05 09:12:17 +0300

    store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,18 +3,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/auth.api";
 import { userProfileApi } from "./api/userProfile.api";
 import { userApi } from "./api/user.api";
+import { serviceApi } from "./api/services.api";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     [userProfileApi.reducerPath]: userProfileApi.reducer,
-    [userApi.reducerPath]: userApi.reducer
+    [userApi.reducerPath]: userApi.reducer,
+    [serviceApi.reducerPath]: serviceApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
         .concat(apiSlice.middleware)
         .concat(userProfileApi.middleware)
-        .concat(userApi.middleware),
+        .concat(userApi.middleware)
+        .concat(serviceApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
