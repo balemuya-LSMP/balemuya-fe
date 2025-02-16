3fac531d8fad884943a8568377db230ce71d001c
EphriamHab <ephremhabtamu2015@gmail.com>
2025-02-16 19:41:32 +0300

    loader

--- src/app/(features)/_components/loader.tsx
+++ src/app/(features)/_components/loader.tsx
@@ -2,10 +2,8 @@
 
 export default function Loader() {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500">
-                
-            </div>
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="w-16 h-16 border-4 border-t-transparent border-gray-500 rounded-full animate-spin"></div>
         </div>
-    )
+    );
 }
