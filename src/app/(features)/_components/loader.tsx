35d79c17a6e7b02a61d11a77bff375028ab3a1e9
EphriamHab <ephremhabtamu2015@gmail.com>
2025-03-26 20:25:17 +0300

    move to locale

--- src/app/(features)/_components/loader.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Loader() {
-    return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <div className="w-16 h-16 border-4 border-t-transparent border-gray-500 rounded-full animate-spin"></div>
-        </div>
-    );
-}
