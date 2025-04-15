e1a05fb24c9bfb922ace7114ec5465334a4cc409
EphriamHab <ephremhabtamu2015@gmail.com>
2025-04-15 19:30:37 +0300

    NotificationPanel

--- src/app/[locale]/professional/_components/NotificationsPanel.tsx
+++ src/app/[locale]/professional/_components/NotificationsPanel.tsx
@@ -96,7 +96,7 @@ export default function NotificationsPanel({ isOpen, onClose }: { isOpen: boolea
 
       {/* Notification Panel */}
       <div
-        className={`fixed top-0 right-0 w-96 h-full bg-white transform ${isOpen ? "translate-x-0" : "translate-x-full"
+        className={`fixed mt-0.5 right-0 w-96 h-full bg-white transform ${isOpen ? "translate-x-0" : "translate-x-full"
           } transition-transform duration-300 ease-in-out z-50 shadow-lg border-l`}
       >
         <div className="h-full flex flex-col">
