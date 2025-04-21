1c8e8414ba0dc82c2b9f92e2d817371d6a169a65
EphriamHab <ephremhabtamu2015@gmail.com>
2025-04-21 19:55:04 +0300

    userSchema

--- src/store/usetSchema.ts
+++ src/store/usetSchema.ts
@@ -1,15 +1,13 @@
 import { z } from "zod";
 
 export const UserSchema = z.object({
-  first_name: z.string().min(2, "First name is required"),
-  middle_name: z.string().optional(),
-  last_name: z.string().min(2, "Last name is required"),
-  email: z.string().email("Invalid email format"),
+  user_name: z.string().min(2, "user name is required"),
+  email: z.string().email("Invalid email address"),
   phone_number: z.string().min(10, "Phone number should be at least 10 digits"),
-  gender: z.enum(["male", "female"], {
-    errorMap: () => ({ message: "Please select a gender" }),
-  }),
   user_type: z.enum(["customer", "professional"], {
+    errorMap: () => ({ message: "Please select a user type" }),
+  }),
+  entity_type: z.enum(["individual", "organization"], {
     errorMap: () => ({ message: "Please select a user type" }),
   }),
   password: z.string().min(8, "Password must be at least 8 characters"),
