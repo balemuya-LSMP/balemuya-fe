a98749bcb7449c467e879ba600d0e6ef4a9e5926
EphriamHab <ephremhabtamu2015@gmail.com>
2025-05-21 19:53:54 +0300

    login

--- src/app/[locale]/auth/login/page.tsx
+++ src/app/[locale]/auth/login/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @next/next/no-img-element */
 "use client";
@@ -39,9 +40,8 @@ export default function Login() {
       } else {
         router.push("/");
       }
-    } catch (err) {
-      console.error("Login failed:", err);
-      toast.error("Login failed");
+    } catch (error: any) {
+      toast.error(error?.data?.error);
     }
   };
 
