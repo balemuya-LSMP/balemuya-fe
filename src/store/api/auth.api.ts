25cfc95a5ef83d607f7347187c5330d9555f8c3a
EphriamHab <ephremhabtamu2015@gmail.com>
2025-05-25 21:18:42 +0300

    auth api

--- src/store/api/auth.api.ts
+++ src/store/api/auth.api.ts
@@ -1,17 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { UserResponse, User, loginState, loginResponse } from '../types';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { UserResponse, User, loginState, loginResponse } from "../types";
 
 export const apiSlice = createApi({
-  reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://balemuya-project.onrender.com/api/users' }),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://balemuya-project.onrender.com/api/users",
+  }),
   endpoints: (builder) => ({
     registerUser: builder.mutation<UserResponse, User>({
       query: (user) => {
         const { confirmPassword, ...userWithoutConfirmPassword } = user;
         return {
-          url: '/auth/register/',
-          method: 'POST',
+          url: "/auth/register/",
+          method: "POST",
           body: userWithoutConfirmPassword,
         };
       },
@@ -19,29 +22,56 @@ export const apiSlice = createApi({
     loginUser: builder.mutation<loginResponse, loginState>({
       query: (user) => {
         return {
-          url: '/auth/login/',
-          method: 'POST',
+          url: "/auth/login/",
+          method: "POST",
           body: user,
         };
       },
     }),
-    googleLogin: builder.mutation<loginResponse, { code: string;}>({
+    googleLogin: builder.mutation<loginResponse, { code: string }>({
       query: ({ code }) => ({
-        url: `/auth/continue-with-google/`, 
-        method: 'POST',
+        url: `/auth/continue-with-google/`,
+        method: "POST",
         body: { code },
       }),
     }),
-    logoutUser: builder.mutation<null,  string >({
+    googleSignup: builder.mutation<
+      loginResponse,
+      { code: string; entity_type: string; user_type: string }
+    >({
+      query: ({ code, entity_type, user_type }) => ({
+        url: `/auth/continue-with-google/`,
+        method: "POST",
+        body: {
+          code,
+          entity_type,
+          user_type,
+        },
+      }),
+    }),
+    logoutUser: builder.mutation<null, string>({
       query: (token) => ({
         headers: {
           Authorization: `Bearer ${token}`,
         },
-        url: '/auth/logout/',
-        method: 'POST',
+        url: "/auth/logout/",
+        method: "POST",
+      }),
+    }),
+    getFeedback: builder.query<any, void>({
+      query: () => ({
+        url: "/feedbacks/ ",
+        method: "GET",
       }),
     }),
   }),
 });
 
-export const { useRegisterUserMutation, useLoginUserMutation, useGoogleLoginMutation, useLogoutUserMutation} = apiSlice;
+export const {
+  useRegisterUserMutation,
+  useLoginUserMutation,
+  useGoogleLoginMutation,
+  useLogoutUserMutation,
+  useGoogleSignupMutation,
+  useGetFeedbackQuery
+} = apiSlice;
